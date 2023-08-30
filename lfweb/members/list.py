f7f7f6e3414781395b00ffe140ac085d96115eb3
Carsten Skov <carsten@simcax.dk>
2023-08-30 21:24:33 +0200

    Fixing linting issues

--- lfweb/members/list.py
+++ lfweb/members/list.py
@@ -1,5 +1,11 @@
+"""Module for handling the memberlist from ForeningLet"""
+
+
 class Members:
+    """The members class provides methods for handling the members list"""
+
     initialized = False
 
     def __init__(self):
+        """Setup the member list"""
         self.initialized = True
--- lfweb/tests/conftest.py
+++ lfweb/tests/conftest.py
@@ -1,7 +1,6 @@
 from os import environ
 
 import pytest
-from flask import current_app
 from testcontainers.redis import RedisContainer
 
 from lfweb import create_app
@@ -12,12 +11,12 @@ def redis_container():
     """
     Start a redis container
     """
-    with RedisContainer("redis:7.0.12") as redis_container:
-        print(f"{redis_container.get_container_host_ip()}")
-        environ["REDIS_PORT"] = redis_container.get_exposed_port("6379")
-        environ["REDIS_HOST"] = redis_container.get_container_host_ip()
+    with RedisContainer("redis:7.0.12") as redis_container_cont:
+        print(f"{redis_container_cont.get_container_host_ip()}")
+        environ["REDIS_PORT"] = redis_container_cont.get_exposed_port("6379")
+        environ["REDIS_HOST"] = redis_container_cont.get_container_host_ip()
 
-        yield redis_container
+        yield redis_container_cont
 
 
 @pytest.fixture
@@ -25,10 +24,10 @@ def client():
     """Providess a client to test with"""
     app = create_app()
 
-    # This will create a redics container on localhost - which is the default for the app factory settings
+    # This will create a redics container on localhost
+    # which is the default for the app factory settings
     with app.test_client() as app_client:
         with app.app_context():
             # None
-            temp = 1
-
+            pass
         yield app_client
