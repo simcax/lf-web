770e0c49387cd7c58d4c88fcc2a1de84e11f404b
Carsten Skov <carsten@simcax.dk>
2024-02-04 21:10:52 +0100

    Changing css framework to tailwind css

--- lfweb/tests/test_member_data.py
+++ lfweb/tests/test_member_data.py
@@ -1,9 +1,18 @@
 """Tests member data retrieval and manipulation - ie. stats"""
 
-from ..members.list import Members
+import pytest
+
+from ..members.list import Memberdata
 
 
 def test_create_member_class_object():
     """Test the members class yields an object - base test"""
-    member_obj = Members()
+    member_obj = Memberdata()
     assert isinstance(member_obj, object) is True
+
+
+@pytest.mark.vcr()
+def test_member_count():
+    """Test that we can retrieve a member count"""
+    memberdata = Memberdata()
+    assert memberdata.members.member_count >= 1
