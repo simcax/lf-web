45ed821d143b93501c330656800969bace20184b
Carsten Skov <carsten@simcax.dk>
2024-04-21 11:43:50 +0200

    Refactoring of the database migration and connection modules.

--- lfweb/tests/test_database.py
+++ lfweb/tests/test_database.py
@@ -5,62 +5,62 @@ from tempfile import TemporaryDirectory
 
 from psycopg import Connection
 
-from lfweb.database.connection import Database, GetDbConnection
+from lfweb.database.connection import DbConnectionCredentials, GetDbConnection
 
 
-def test_create_database_object():
-    """Test that we can create a database object"""
-    db = Database()
-    assert isinstance(db, Database) is True
+def test_create_db_connection_credentials_from_env_vars(postgres_container, db_creds):
+    """Test that we can create a database connection credentials object from env vars"""
+    assert db_creds.host == os.getenv("DB_HOST")
+    assert db_creds.port == os.getenv("DB_PORT")
+    assert db_creds.username == os.getenv("DB_USERNAME")
+    assert db_creds.password == os.getenv("DB_PASSWORD")
+    assert db_creds.database == os.getenv("DB_NAME")
 
 
-def test_pages_table_exists():
+def test_pages_table_exists(postgres_container, init_db, db_creds):
     """Test that the pages table exists"""
-    db = Database("ourtestdb")
-    db.migration()
-    with GetDbConnection(db) as connection:
-        cur = connection.cursor()
+    with GetDbConnection(db_creds) as conn:
+        cur = conn.cursor()
         cur.execute("SELECT * FROM pages")
         rows = cur.fetchall()
         assert rows is not None
 
 
-def test_connect_to_database():
+def test_connect_to_database(postgres_container, db_creds):
     """Test that we can connect to the database"""
-    with GetDbConnection(True) as connection:
+    with GetDbConnection(db_creds) as connection:
         assert isinstance(connection, Connection)
 
 
-def test_create_pages_table():
-    """Test that we can create a pages table in the database"""
-    database = Database(True)
-    table_created = database.create_pages_table()
-    assert table_created is True
+# def test_add_a_page_to_the_database():
+#     """Test that we can add a page to the database"""
+#     db_creds = DbConnectionCredentials()
+#     with GetDbConnection(db_creds) as cur:
+#     page = {"title": "Test Page", "md": "test-page.md", "url": "/test-page"}
+#     tempdir = TemporaryDirectory()
+#     with tempdir as tmp:
+#         page_path = os.path.join(tmp, page.get("md"))
+#         with open(page_path, "w", encoding="utf-8") as file:
+#             file.write("This is a test page.")
+#             page_added = database.add_page(page)
+#     assert page_added is True
 
+# def test_add_page_to_database(postgres_container):
+#     """Tests adding a page to the pages table by a method in the pages module"""
+#     db = DatabaseMigration()
+#     db.create_database()
+#     with db.connection as conn:
+#         db.run_migrations()
+#         # Assuming there is a method in the pages module called "add_page"
+#         # that adds a page to the pages table
+#         pdh = PagesDatabaseHandling()
+#         pdh.add_page_to_index(
+#             "Test Page", "/testpage", "testpage.md", False
+#         )  # TODO The MD field should be an id pointing to the id of the md table
 
-def test_create_pages_table_already_created():
-    """Test that we don't get an error if the table already exists"""
-    database = Database(False)
-    table_created = database.create_pages_table()
-    table_created_2 = database.create_pages_table()
-    assert table_created_2 is True
-
-
-def test_create_index_table():
-    """Test that we can create an index table in the database"""
-    database = Database(True)
-    table_created = database.create_index_table()
-    assert table_created is True
-
-
-def test_add_a_page_to_the_database():
-    """Test that we can add a page to the database"""
-    database = Database(True)
-    page = {"title": "Test Page", "md": "test-page.md", "url": "/test-page"}
-    tempdir = TemporaryDirectory()
-    with tempdir as tmp:
-        page_path = os.path.join(tmp, page.get("md"))
-        with open(page_path, "w", encoding="utf-8") as file:
-            file.write("This is a test page.")
-            page_added = database.add_page(page)
-    assert page_added is True
+#         # Check if the page was added successfully
+#         pages_query = conn.execute(
+#             text("SELECT * FROM pages WHERE title = 'Test Page'")
+#         )
+#         rows = pages_query.fetchall()
+#         assert len(rows) == 1
--- lfweb/tests/test_database_migrations.py
+++ lfweb/tests/test_database_migrations.py
@@ -17,7 +17,6 @@ def test_create_database_migration_object_with_args(postgres_container):
     """Test that we can create a database migration object with args"""
     db = DatabaseMigration()
     assert isinstance(db, DatabaseMigration) is True
-    assert postgres_container.host in db.engine.url.host
 
 
 def test_database_being_created(postgres_container):
@@ -28,7 +27,7 @@ def test_database_being_created(postgres_container):
         databases_query = conn.execute(text("SELECT datname FROM pg_database"))
         rows = databases_query.fetchall()
         databases = [row[0] for row in rows]
-        assert postgres_container.database in databases
+        assert postgres_container.POSTGRES_DB in databases
 
 
 def test_create_tables(postgres_container):
@@ -47,22 +46,20 @@ def test_create_tables(postgres_container):
         assert "pages" in tables
 
 
-def test_add_page_to_database(postgres_container):
-    """Tests adding a page to the pages table by a method in the pages module"""
+def test_drop_tables(postgres_container):
+    """Tests dropping the tables from the database"""
     db = DatabaseMigration()
     db.create_database()
     with db.connection as conn:
         db.run_migrations()
-        # Assuming there is a method in the pages module called "add_page"
-        # that adds a page to the pages table
-        pdh = PagesDatabaseHandling()
-        pdh.add_page_to_index(
-            "Test Page", "/testpage", "testpage.md", False
-        )  # TODO The MD field should be an id pointing to the id of the md table
-
-        # Check if the page was added successfully
-        pages_query = conn.execute(
-            text("SELECT * FROM pages WHERE title = 'Test Page'")
+        db.drop_tables()
+        tables_query = conn.execute(
+            text(
+                "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+            )
         )
-        rows = pages_query.fetchall()
-        assert len(rows) == 1
+        rows = tables_query.fetchall()
+        tables = [row[0] for row in rows]
+        assert "pages" not in tables
+        assert "index" not in tables
+        assert "md" not in tables
