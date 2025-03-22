87aab3f6df3517377083dfd51885b57fa3635a7f
Carsten Skov <carsten@simcax.dk>
2025-03-22 22:36:04 +0100

    Removing commented code

--- lfweb/main/__init__.py
+++ lfweb/main/__init__.py
@@ -8,10 +8,3 @@ from loguru import logger
 from .images import bp as images_bp
 from .pages_route import bp as pages_bp
 from .routes import frontpage_bp
-
-# bp = Blueprint("pages", __name__, url_prefix="/")
-
-# from lfweb.main import routes  # pylint: disable=wrong-import-position
-
-
-# bp = Blueprint("pages", __name__, url_prefix="/")
