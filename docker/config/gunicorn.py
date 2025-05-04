7dbcb6cd4bbe3938edc27aa21fadd52e7e34d4ee
Carsten Skov <carsten@simcax.dk>
2025-05-04 16:10:32 +0200

    Configures Gunicorn using a config file
    Configures Gunicorn using a dedicated configuration file for better management of settings.
    
    This change decouples Gunicorn configuration from the Dockerfile, making it easier to modify settings without rebuilding the container.

--- docker/config/gunicorn.py
+++ docker/config/gunicorn.py
@@ -1,16 +1,18 @@
 # Made from https://github.com/nickjj/build-a-saas-app-with-flask
 import multiprocessing
 import os
-from distutils.util import strtobool
 
 bind = os.getenv("WEB_BIND", "0.0.0.0:8000")
 accesslog = "-"
-access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+access_log_format = (
+    "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+)
 
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
 threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
 
-reload = bool(strtobool(os.getenv("WEB_RELOAD", "false")))
+
+reload = os.getenv("WEB_RELOAD", "false").lower() in ("true", "1")
 
 limit_request_line = 0
 limit_request_field_size = 0
